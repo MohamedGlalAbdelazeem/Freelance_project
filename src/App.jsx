154af8e46750ebfeb00ed88e4ff0c18a30d77a06
Alaa-Eldeyn <alaa.eldeyn@gmail.com>
2024-04-26 02:11:00 +0300

    fetch & store employees

--- src/App.jsx
+++ src/App.jsx
@@ -5,20 +5,20 @@ import { Routes , Route } from "react-router-dom"
 import Userpage from "./components/asidepages/userpage/Userpage"
 import Branchpage from "./components/asidepages/branchpage/Branchpage"
 import Userprofilepage from "./components/header/Userprofilepage"
- 
- 
+
+
 function App() {
   
 
   return (
     <>
-     <Routes>
+    <Routes>
         <Route path="/" element={<Login />} />
         <Route path="/Login" element={<Login />} />
         <Route path="/Forgetpasssword" element={<Forgetpasssword />} />
         <Route path="/Mainpage" element={<Mainpage />}>
-           <Route path="Userpage" element={<Userpage />} />
-           <Route path="Branchpage" element={<Branchpage />} />
+          <Route path="Userpage" element={<Userpage />} />
+          <Route path="Branchpage" element={<Branchpage />} />
           <Route path="Userprofilepage" element={<Userprofilepage />} />
           
         </Route>
