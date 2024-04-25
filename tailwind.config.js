9cda9f6f5880d5cf401607cf6ad2f0321ef4882f
Alaa-Eldeyn <alaa.eldeyn@gmail.com>
2024-04-25 15:18:19 +0200

    theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,16 +1,14 @@
  /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  darkMode: "class",
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {},
   },
-   plugins: [require("daisyui")],
-   daisyui: {
+  plugins: [require("daisyui")],
+  daisyui: {
     themes: false, // false: only light + dark | true: all themes | array: specific themes like this ["light", "dark", "cupcake"]
-    darkTheme: "ligth", // name of one of the included themes for dark mode
+    darkTheme: "light", // name of one of the included themes for dark mode
     base: true, // applies background color and foreground color for root element by default
     styled: true, // include daisyUI colors and design decisions for all components
     utils: true, // adds responsive and modifier utility classes
@@ -18,5 +16,5 @@ export default {
     logs: true, // Shows info about daisyUI version and used config in the console when building your CSS
     themeRoot: ":root", // The element that receives theme color CSS variables
   },
-}
+};
  
