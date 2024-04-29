8db05c0bcb7d507ac3b8a60b03565851192c30f8
Alaa-Eldeyn <alaa.eldeyn@gmail.com>
2024-04-29 03:23:28 +0300

    handle Search

--- src/components/asidepages/branchpage/Branchpage.jsx
+++ src/components/asidepages/branchpage/Branchpage.jsx
@@ -219,24 +219,22 @@ function Branchpage() {
   };
 
   const handleSearch = (e) => {
-    setLoader(true);
     e.preventDefault();
-    axios
-      .get(`${baseUrl}branches/${searchValue}`, {
-        headers: {
-          Authorization: `Bearer ${userToken}`,
-        },
-      })
-      .then((response) => {
-        setBranches([response.data.data]);
-      })
-      .catch((error) => {
-        console.error("Error fetching branches:", error);
-        toast(error.response.data.message);
-      })
-      .finally(() => {
-        setLoader(false);
-      });
+    setLoader(true);
+
+    if (!searchValue.trim()) {
+      fetchBranches();
+      return;
+    }
+    let allBranches = [...branches];
+    let filteredBranches = [];
+    allBranches.forEach((Branch) => {
+      if (Branch.name.toLowerCase().includes(searchValue.toLowerCase())) {
+        filteredBranches.push(Branch);
+      }
+    });
+    setBranches(filteredBranches);
+    setLoader(false);
   };
 
   return (
@@ -378,6 +376,7 @@ function Branchpage() {
               id="default-search"
               value={searchValue}
               onChange={(e) => setSearchValue(e.target.value)}
+              onKeyUp={(e) => handleSearch(e)}
               className="block w-full p-4 pb-4 ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
               placeholder="البحث من عن طريق ID "
               required
@@ -539,35 +538,34 @@ function Branchpage() {
       {loader && <div className="spinner"></div>}
       {/* Pagination */}
 
-    
-              <div className="max-w-full md:max-w-screen-md lg:max-w-screen-lg xl:max-w-screen-xl mx-auto bg-white p-6 rounded-lg shadow-sm">
+      <div className="max-w-full md:max-w-screen-md lg:max-w-screen-lg xl:max-w-screen-xl mx-auto bg-white p-6 rounded-lg shadow-sm">
+        <div className="flex justify-center">
+          <nav className="flex space-x-2" aria-label="Pagination">
+            <a
+              href="#"
+              className="relative inline-flex items-center px-4 py-2 text-sm bg-gradient-to-r from-violet-300 to-indigo-300 border border-fuchsia-100 hover:border-violet-100 text-white font-semibold cursor-pointer leading-5 rounded-md transition duration-150 ease-in-out focus:outline-none focus:shadow-outline-blue focus:border-blue-300 focus:z-10"
+            >
+              Previous
+            </a>
+            {Array.from({ length: NbPage }, (_, i) => i + 1).map((page) => (
+              <a
+                key={page} // Make sure to include a unique key for each element in the array
+                href="#"
+                className="relative inline-flex items-center px-4 py-2 text-sm font-medium text-gray-700 bg-white border border-fuchsia-100 hover:bg-fuchsia-200 cursor-pointer leading-5 rounded-md transition duration-150 ease-in-out focus:outline-none focus:shadow-outline-blue focus:border-blue-300 focus:z-10"
+              >
+                {page}
+              </a>
+            ))}
 
-                  <div className="flex justify-center">
-                      <nav className="flex space-x-2" aria-label="Pagination">
-                          <a href="#" className="relative inline-flex items-center px-4 py-2 text-sm bg-gradient-to-r from-violet-300 to-indigo-300 border border-fuchsia-100 hover:border-violet-100 text-white font-semibold cursor-pointer leading-5 rounded-md transition duration-150 ease-in-out focus:outline-none focus:shadow-outline-blue focus:border-blue-300 focus:z-10">
-                              Previous
-                          </a>
-                          {Array.from({ length: NbPage }, (_, i) => i + 1).map((page) => (
-                            <a
-                              key={page} // Make sure to include a unique key for each element in the array
-                              href="#"
-                              className="relative inline-flex items-center px-4 py-2 text-sm font-medium text-gray-700 bg-white border border-fuchsia-100 hover:bg-fuchsia-200 cursor-pointer leading-5 rounded-md transition duration-150 ease-in-out focus:outline-none focus:shadow-outline-blue focus:border-blue-300 focus:z-10"
-                            >
-                              {page}
-                            </a>
-                          ))}
-
-                        
-                          <a   href="#" className="relative inline-flex items-center px-4 py-2 text-sm bg-gradient-to-r from-violet-300 to-indigo-300 border border-fuchsia-100 hover:border-violet-100 text-white font-semibold cursor-pointer leading-5 rounded-md transition duration-150 ease-in-out focus:outline-none focus:shadow-outline-blue focus:border-blue-300 focus:z-10">
-                              Next
-                          </a>
-                      </nav>
-                  </div>
-              </div>
-    
-                
-
-
+            <a
+              href="#"
+              className="relative inline-flex items-center px-4 py-2 text-sm bg-gradient-to-r from-violet-300 to-indigo-300 border border-fuchsia-100 hover:border-violet-100 text-white font-semibold cursor-pointer leading-5 rounded-md transition duration-150 ease-in-out focus:outline-none focus:shadow-outline-blue focus:border-blue-300 focus:z-10"
+            >
+              Next
+            </a>
+          </nav>
+        </div>
+      </div>
     </main>
   );
 }
