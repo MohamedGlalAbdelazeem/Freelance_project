3c9f2d080a5338e469d3f9538e17caedf03d283c
Alaa-Eldeyn <alaa.eldeyn@gmail.com>
2024-04-30 18:16:55 +0300

    V.2

--- src/components/header/Userprofilepage.jsx
+++ src/components/header/Userprofilepage.jsx
@@ -3,39 +3,32 @@ import { useState, useEffect } from "react";
 import userPhoto from "./user.avif";
 import { useNavigate } from "react-router-dom";
 import { toast } from "react-toastify";
-import { useForm } from "react-hook-form";
-import { z } from "zod";
-import { zodResolver } from "@hookform/resolvers/zod";
 
-function Userprofilepage() {
-    
+function UserProfilePage() {
   const [userProfile, setUserprofile] = useState([]);
   const [loader, setLoader] = useState(true);
-  const navigate = useNavigate(); // Renamed useNavigate to navigate to avoid confusion with component
+  const navigate = useNavigate();
+  const userToken = localStorage.getItem("user_token");
+  const [name, setName] = useState("");
+  const [phoneNumber, setPhoneNumber] = useState("");
 
-  const schema = z.object({
-    name: z.string().min(1, "الاسم يجب ان يكون 4 احرف على الاقل"),
-    phone_number: z.string().min("ادخل رقم الهاتف"),
-  });
-
-  const {
-    register,
-    handleSubmit,
-    setValue,
-    reset,
-    getValues,
-    formState: { errors, isSubmitting },
-  } = useForm({ resolver: zodResolver(schema) });
+  const [oldPassword, setOldPassword] = useState("");
+  const [newPassword, setNewPassword] = useState("");
+  const [confirmNewPassword, setConfirmNewPassword] = useState("");
+  const [isError, setIsError] = useState(false);
 
   useEffect(() => {
+    setLoader(true);
     const userRoleName = localStorage.getItem("user_role_name");
     const userToken = localStorage.getItem("user_token");
-
     if (!userToken) {
       handleUnauthenticated();
       return;
     }
+    refreshUser();
+  }, []);
 
+  const refreshUser = () => {
     axios
       .get("http://127.0.0.1:8000/api/refresh", {
         headers: {
@@ -45,30 +38,35 @@ function Userprofilepage() {
       .then(function (response) {
         setLoader(false);
         setUserprofile(response.data.Admin);
-        console.log(response.data.Admin);
+        setName(response.data.Admin.name);
+        setPhoneNumber(response.data.Admin.phone_number);
       })
       .catch(function (error) {
         if (error.response && error.response.status === 401) {
           handleUnauthenticated();
         } else {
-          // Handle other errors
           console.error("Error refreshing token:", error);
         }
-
-      }).finally(() => {
+      })
+      .finally(() => {
         setLoader(false);
-     console.log(userProfile);
-      });   
-  }, [])
-  //test
-  const handleUpdate = () => {
+        console.log(userProfile);
+      });
+  };
+
+  const handleUpdate = (e) => {
+    e.preventDefault();
+    if (!name || !phoneNumber) {
+      toast.error("الرجاء ملء جميع الحقول");
+      return;
+    }
     setLoader(true);
     axios
       .post(
-        `${baseUrl}update`,
+        `http://127.0.0.1:8000/api/update`,
         {
-          name: getValues("name"),
-          phone_number: getValues("phone_number"),
+          name: name,
+          phone_number: phoneNumber,
         },
         {
           headers: {
@@ -76,12 +74,55 @@ function Userprofilepage() {
           },
         }
       )
-      .then(function () {
+      .then(function (res) {
         toast.success("تم تحديث البيانات بنجاح");
-        setUpdateMode(false);
+        refreshUser();
       })
       .catch(function (error) {
-        toast.error(error.response.data.message);
+        toast.error("لم يتم تعديل البيانات");
+        console.log(error);
+      })
+      .finally(() => {
+        setLoader(false);
+      });
+  };
+
+  const changePassword = async (e) => {
+    setLoader(true);
+    e.preventDefault();
+    if (newPassword.trim() === "" && confirmNewPassword.trim() === "") {
+      setIsError(true);
+      return;
+    }
+    if (newPassword.trim() !== confirmNewPassword.trim()) {
+      toast("كلمة المرور غير متطابقة", { type: "error" });
+      return;
+    }
+    if (newPassword.trim().length < 6 && confirmNewPassword.trim().length < 6) {
+      setIsError(true);
+      return;
+    }
+    await axios
+      .post(
+        "http://127.0.0.1:8000/api/change-password",
+        {
+          current_password: oldPassword,
+          password: newPassword,
+          password_confirmation: confirmNewPassword,
+        },
+        {
+          headers: {
+            Authorization: `Bearer ${userToken}`,
+          },
+        }
+      )
+      .then((res) => {
+        if (res.status === 200) {
+          toast("تم تغيير كلمة السر بنجاح", { type: "success" });
+        }
+      })
+      .catch((error) => {
+        toast(error.response.data.message, { type: "error" });
       })
       .finally(() => {
         setLoader(false);
@@ -104,15 +145,27 @@ function Userprofilepage() {
         <form>
           <div className="mb-2 pt-2">
             <div>
-              <div className="flex justify-center items-center">
+              <div className="flex justify-center items-center flex-col mb-10">
                 <img
                   src={userPhoto}
-                  className="border-double border-4 border-sky-500 w-36 rounded-full h-36 mb-8"
+                  className="border-double border-4 border-sky-500 w-36 rounded-full h-36 "
                   alt="user_profile_photo"
-                />{" "}
+                />
+                <p className="text-yellow-600">Demo</p>
+                <p>
+                  <strong>الاسم: </strong>
+                  {userProfile.name}
+                </p>
+                <p>
+                  <strong>رقم الهاتف: </strong>
+                  {userProfile.phone_number}
+                </p>
               </div>
               <input type="file" name="file" id="file" className="sr-only" />
-              <label className="cursor-pointer relative flex min-h-[50px] items-center justify-center rounded-xl border border-dashed border-[#e0e0e0] p-5 text-center">
+              <label
+                htmlFor="file"
+                className="cursor-pointer relative flex min-h-[50px] items-center justify-center rounded-xl border border-dashed border-[#e0e0e0] p-5 text-center"
+              >
                 <div>
                   <span className="mb-2 block text-sm font-semibold text-[#07074D]">
                     تغيير الصورة الشخصية
@@ -133,7 +186,7 @@ function Userprofilepage() {
       </div>
       <h3 className="flex items-center w-full">
         <span className="flex-grow bg-gray-200 rounded h-1"></span>
-        <button className="mx-2 text-md font-medium  border-2 rounded-full hover:bg-gray-200">
+        <button className="mx-2 px-3 py-1 text-md font-medium  border-2 rounded-full hover:bg-gray-200">
           تعديل البيانات الشخصية
         </button>
         <span className="flex-grow bg-gray-200 rounded h-1"></span>
@@ -146,8 +199,9 @@ function Userprofilepage() {
               <label className="mb-3 block text-base font-medium ">الاسم</label>
               <input
                 type="text"
-                {...register("name")}
-                 value={userProfile.name}
+                value={name}
+                onChange={(e) => setName(e.target.value)}
+                defaultValue={loader ? userProfile?.name : ""}
                 className="w-full rounded-md border border-[#e0e0e0] bg-white py-3 px-6 text-base font-medium text-[#6B7280] outline-none focus:border-[#6A64F1] focus:shadow-md"
               />
             </div>
@@ -156,15 +210,18 @@ function Userprofilepage() {
                 رقم الهاتف
               </label>
               <input
-                type="number"
-                {...register("phone_number")}
-                value={userProfile.phone_number}
+                type="text"
+                value={phoneNumber}
+                onChange={(e) => setPhoneNumber(e.target.value)}
+                defaultValue={loader ? userProfile?.phone_number : ""}
                 className="w-full rounded-md border border-[#e0e0e0] bg-white py-3 px-6 text-base font-medium text-[#6B7280] outline-none focus:border-[#6A64F1] focus:shadow-md"
               />
             </div>
             <div>
               <button
-                onClick={handleUpdate}
+                onClick={(e) => {
+                  handleUpdate(e);
+                }}
                 className="hover:shadow-form rounded-md bg-success hover:bg-success/90 py-3 px-8 text-base font-semibold text-white outline-none"
               >
                 تعديل
@@ -193,6 +250,8 @@ function Userprofilepage() {
                 type="password"
                 name="name"
                 id="name"
+                value={oldPassword}
+                onChange={(e) => setOldPassword(e.target.value)}
                 className="w-full rounded-md border border-[#e0e0e0] bg-white py-3 px-6 text-base font-medium text-[#6B7280] outline-none focus:border-[#6A64F1] focus:shadow-md"
               />
             </div>
@@ -202,8 +261,15 @@ function Userprofilepage() {
               </label>
               <input
                 type="password"
+                value={newPassword}
+                onChange={(e) => setNewPassword(e.target.value)}
                 className="w-full rounded-md border border-[#e0e0e0] bg-white py-3 px-6 text-base font-medium text-[#6B7280] outline-none focus:border-[#6A64F1] focus:shadow-md"
               />
+              {isError && (
+                <p className="text-red-500 text-sm">
+                  كلمة المرور يجب ان تكون اكبر من 6 احرف
+                </p>
+              )}
             </div>
 
             <div className="mb-5">
@@ -212,12 +278,22 @@ function Userprofilepage() {
               </label>
               <input
                 type="password"
+                value={confirmNewPassword}
+                onChange={(e) => setConfirmNewPassword(e.target.value)}
                 className="w-full rounded-md border border-[#e0e0e0] bg-white py-3 px-6 text-base font-medium text-[#6B7280] outline-none focus:border-[#6A64F1] focus:shadow-md"
               />
+              {isError && (
+                <p className="text-red-500 text-sm">
+                  كلمة المرور يجب ان تكون اكبر من 6 احرف
+                </p>
+              )}
             </div>
 
             <div>
-              <button className="hover:shadow-form rounded-md bg-success hover:bg-success/90 py-3 px-8 text-base font-semibold text-white outline-none">
+              <button
+                onClick={(e) => changePassword(e)}
+                className="hover:shadow-form rounded-md bg-success hover:bg-success/90 py-3 px-8 text-base font-semibold text-white outline-none"
+              >
                 تغيير
               </button>
             </div>
@@ -228,4 +304,4 @@ function Userprofilepage() {
   );
 }
 
-export default Userprofilepage;
+export default UserProfilePage;
