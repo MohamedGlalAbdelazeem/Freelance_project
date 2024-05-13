714a7179115bd4c7ebf773416860492614cea696
Alaa-Eldeyn <alaa.eldeyn@gmail.com>
2024-05-13 04:34:01 +0300

    ui

--- src/components/asidepages/bookingPage/BookingPage.jsx
+++ src/components/asidepages/bookingPage/BookingPage.jsx
@@ -8,21 +8,24 @@ const BookingPage = () => {
         اختر خدمة
       </p>
       <section className="flex justify-evenly flex-col md:flex-row gap-5 items-center text-center min-h-[80vh]">
-        <Link
-          to={"trip"}
-          className="w-1/2 max-w-[300px] rounded-lg h-[180px] p-5 flex justify-center items-center text-3xl font-bold cursor-pointer hover:scale-105 hover:tracking-wider transition-all duration-300 ease-in-out hover:bg-gray-700 hover:shadow-lg bg-gray-900 text-white"
-        >
-          حجز رحلة
-          <ConnectingAirportsIcon sx={{ fontSize: 55 }} />
-        </Link>
-
-        <Link
-          to={"service"}
-          className="w-1/2 max-w-[350px] rounded-lg h-[180px] p-5 flex justify-center items-center text-3xl font-bold cursor-pointer hover:scale-105 hover:tracking-wider transition-all duration-300 ease-in-out hover:bg-gray-700 hover:shadow-lg bg-gray-900 text-white"
-        >
-          حجز خدمات أخري
-          <ManageAccountsIcon sx={{ fontSize: 55 }} />
-        </Link>
+        <div className="w-1/2 max-w-[400px] rounded-lg h-[200px] p-5 flex justify-center items-center text-3xl font-bold cursor-pointer hover:scale-105 hover:tracking-wider transition-all duration-300 ease-in-out hover:bg-gray-700 hover:shadow-lg bg-gray-900 text-white">
+          <Link
+            to={"trip"}
+            className="w-full h-full flex justify-center items-center"
+          >
+            حجز رحلة
+            <ConnectingAirportsIcon sx={{ fontSize: 55 }} />
+          </Link>
+        </div>
+        <div className="w-1/2 max-w-[400px] rounded-lg h-[200px] p-5 flex justify-center items-center text-3xl font-bold cursor-pointer hover:scale-105 hover:tracking-wider transition-all duration-300 ease-in-out hover:bg-gray-700 hover:shadow-lg bg-gray-900 text-white">
+          <Link
+            to={"service"}
+            className="w-full h-full flex justify-center items-center"
+          >
+            حجز خدمات أخري
+            <ManageAccountsIcon sx={{ fontSize: 55 }} />
+          </Link>
+        </div>
       </section>
     </>
   );
